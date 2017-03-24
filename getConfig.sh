c16459d24f4b4d9d94918ddfa9cbecaa36cefa46
brett-grosvenor <brett.grosvenor@parivedasolutions.com>
2017-03-24 18:27:43 -0500

    Revert "Initial commit of brett into master branch"
    This reverts commit 66b7947b2a4cbc3f5bb1bb170b528f4a71fe45ae.

--- getConfig.sh
+++ getConfig.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-aws cloudformation list-exports --query 'Exports[?Name==`brett-security-'"$STAGE"'`].Value' --output text > lambdarolearn.txt
-aws cloudformation list-exports --query 'Exports[?Name==`brett-security-secrets-'"$STAGE"'`].Value' --output text > s3.txt
+aws cloudformation list-exports --query 'Exports[?Name==`template-security-'"$STAGE"'`].Value' --output text > lambdarolearn.txt
+aws cloudformation list-exports --query 'Exports[?Name==`template-security-secrets-'"$STAGE"'`].Value' --output text > s3.txt
 aws codepipeline --region us-east-1 get-pipeline-state --name $PIPELINE | jq -r '.stageStates[0].actionStates[0].currentRevision.revisionId' > securityhash.txt
 aws codepipeline --region us-east-1 get-pipeline-state --name $PIPELINE | jq -r '.stageStates[0].actionStates[1].currentRevision.revisionId' > apphash.txt
